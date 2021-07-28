a0301cbcd0e800c05e0654f09ac09f5b5cfebceb
alokkjnu <ry7069577@gmail.com>
2021-07-28 23:11:57 +0530

    commit

--- 5 IteratingInReverse.py
+++ 5 IteratingInReverse.py
@@ -7,3 +7,19 @@ for x in reversed(a):
 f = open('msg.txt')
 for line in reversed(list(f)):
     print(line, end='')
+
+class countdown:
+    def __init__(self,start):
+        self.start = start
+
+    def __iter__(self):
+        n = self.start
+        while n>0:
+            yield n
+            n -= 1
+
+    def __reversed__(self):
+        n = 1
+        while n <=self.start:
+            yield n
+            n +=1
