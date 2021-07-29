416beb4e8d334b7df71eaa13e26a4950174ac149
alokkjnu <ry7069577@gmail.com>
2021-07-29 23:55:30 +0530

    commit

--- 8 SkippingFirstPart.py
+++ 8 SkippingFirstPart.py
@@ -9,6 +9,3 @@ with open('msg.txt') as f:
     for line in dropwhile(lambda line: line.startswith('#'),f):
         print(line, end='')
 
-items = ['a','b','c',1,4,10,15]
-for x in islice(items,3,None):
-    print(x)
